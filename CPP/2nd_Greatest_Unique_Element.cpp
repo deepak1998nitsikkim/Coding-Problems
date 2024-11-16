5b83d4d2fd7f419bfe5b7ee898ff54f5dbde465d
Deepak Dhingan <135021817+deepak1998nitsikkim@users.noreply.github.com>
2024-11-16 23:45:33 +0530

    Create 2nd_Greatest_Unique_Element.cpp

---
+++ CPP/2nd_Greatest_Unique_Element.cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int m1,m2;
+    int a[5]={2,20,18,8,20};
+    
+    if(a[0]>a[1])
+    {
+        m1=a[0];
+        m2=a[1];
+    }
+    
+    else
+    {
+        m1=a[1];
+        m2=a[0];
+    }
+    
+    for(int i=2;i<5;i++)
+    {
+        if(a[i]>m1)
+        {
+            m2=m1;
+            m1=a[i];
+        }
+        else if(a[i]>m2 && a[i]!=m1)
+        {
+            m2=a[i];
+        }
+    }
+    cout<<m2;
+
+    return 0;
+}
